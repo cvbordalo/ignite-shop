e665d8fcdb2ab5630be30ad805bd422e60ff7862
Carlos Bordalo <carlosvitor6@hotmail.com>
2023-02-06 22:57:07 -0300

    Add Header

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+import { globalStyles } from '../styles/global';
+
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
+
+export default App;
